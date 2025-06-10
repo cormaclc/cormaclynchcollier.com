577fbbc0e4b93498c20957127fd47655ce7e8d15
Cormac Lynch-Collier <cormac.collier@gmail.com>
2025-06-09 20:57:18 -0400

    feat: the most basic of sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StateChangeComponent } from '../components/StateChangeComponent';
 import { Navigation } from '../components/Navigation';
+import './index.scss';
 
 export default function Page() {
   return <div>

53c0288d791f9442e78a80a6a19357e6c7cac208
Cormac Lynch-Collier <cormac.collier@gmail.com>
2026-02-07 13:20:05 -0500

    feat: and-natalie page with base styles

---
+++ src/pages/and-natalie/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// styles
+import '../../styles/wedding/base.scss';
+
+const AndNatalie = () => {
+  return <div>
+    We are getting married!
+  </div>;
+};
+
+export default AndNatalie;

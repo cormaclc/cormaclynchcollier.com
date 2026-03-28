7fd7b8efd1e13981df4e8049a60d2c352acfbc05
Cormac Lynch-Collier <cormac.collier@gmail.com>
2026-03-28 13:12:56 -0400

    chore: real accomodation links

--- src/content/wedding/accommodation.ts
+++ src/content/wedding/accommodation.ts
@@ -3,7 +3,7 @@ import type { AccommodationSpec } from '../../types/wedding/accommodation.types'
 const AWOL: AccommodationSpec = {
   name: 'AWOL Stowe',
   details: ['Stay at the ceremony location with us!',  'Book both Friday and Saturday night and get Friday night on us!*'],
-  bookingLink: 'https://www.awolstowe.com/', // TODO: real link...
+  bookingLink: 'https://app.mews.com/distributor/1b454b71-6ea3-43e4-83a4-b27a01047026?mewsAvailabilityBlockId=6ac1ac7d-dcf0-4fc5-bf5b-b3590138feee&mewsStart=2026-08-28&mewsEnd=2026-08-30',
   address: [
     '691 Mountain Rd',
     'Stowe, VT 05672',
@@ -15,7 +15,7 @@ const AWOL: AccommodationSpec = {
 const CADY_HILL: AccommodationSpec = {
   name: 'Cady Hill Lodge',
   details: ['Cady Hill is nextdoor to AWOL.'],
-  bookingLink: 'https://www.cadyhill.com/', // TODO: real link...
+  bookingLink: 'https://app.mews.com/distributor/edc92dd9-d4b6-438c-8ed9-b27a01128a42?mewsAvailabilityBlockId=ec49694d-74b1-43c7-8cde-b3ec0107ddbf&mewsStart=2026-08-28&mewsEnd=2026-08-30',
   address: [
     '511 Mountain Rd',
     'Stowe, VT 05672',
